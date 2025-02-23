2eb0036a63837fa43dd4f68e96937cd2e0cd0332
Cássio Cintra <cassiocr2012@hotmail.com>
2025-02-23 09:22:41 -0300

    remove unused tests

--- ms-shopping-cart/src/test/java/tg/espetinho/ShoppingCartApplicationTests.java
+++ ms-shopping-cart/src/test/java/tg/espetinho/ShoppingCartApplicationTests.java
@@ -1,13 +1,8 @@
 package tg.espetinho;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ShoppingCartApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
